d079adeac35f976f6353fa6c5775b491b905d8e5
tristan.sebens <trisan.ng.sebens@gmail.com>
2018-03-22 13:05:51 -0800

    Debugged and cleaned up heading vector generation

--- data.py
+++ data.py
@@ -19,7 +19,7 @@ def noise(fact=1.0):
 
 
 '''
-Generate semi noisy location data based on a mathematical function
+Generate semi noisy location ais_data based on a mathematical function
 '''
 def gen_loc_data(base_func, noise_fact, n):
     loc_data = []
@@ -32,41 +32,62 @@ def gen_loc_data(base_func, noise_fact, n):
 
 
 '''
-Generate semi noisy heading and SoG data based on location data
+Generate semi noisy heading and SoG ais_data based on location ais_data
 '''
-def gen_head_and_SoG_data_from_loc(loc_data, noise_fact=0.1):
+def gen_head_and_SoG_data_from_loc(loc_data, noise_fact=0.1, display_vectors=False):
     prev = loc_data[0] # Get the initial state
     headings = []
     SoGs = []
     for curr in loc_data[1:]: # Skip the first location, because we've already stored it in prev
         delta_vector = np.subtract(curr, prev) # The exact change of location as a vector (curr-prev=delta)
-        print('p:%s, %s\nc:%s, %s\ndv:%s, %s' % (prev[0], prev[1], curr[0], curr[1],
-                                                 delta_vector[0], delta_vector[1]))
-        plot.axes().quiver(curr[0], curr[1], delta_vector[0], delta_vector[1], color='g')
-        plot.pause(.5)
-        prev = curr # Reset the pointers for the next iteration
         # The SoG is equal to the magnitude of the delta_vector,
         # and the heading is the unit vector form of the delta vector.
         SoG = np.linalg.norm(delta_vector)
         # Now we generate a 'noised' heading vector, which has been pushed in a random direction.
-        # The magnitude of the noise introduced to the heading is within +/- noise_fact of the delta vector magnitude
-        heading_noise = np.array([noise(), noise()])
-        plot.axes().quiver(curr[0], curr[1], heading_noise[0], heading_noise[1], color='k')
-        plot.pause(.5)
+        # The magnitude of the noise introduced to the heading is within +/- noise_fact of the delta-vector magnitude
+        # This ensures that the noise factor is always proportional to the delta-vector, no matter the heading's size.
+        heading_noise = np.array([noise(SoG*noise_fact), noise(SoG*noise_fact)])
+        # Now that we have both the delta-vector and the noise factor
         heading = np.add(delta_vector, heading_noise) # First add the heading noise to the delta vector
         heading = np.divide(heading, np.linalg.norm(heading)) # Then normalize the 'noised' heading vector into a unit vector
-        plot.axes().quiver(curr[0], curr[1], heading[0], heading[1], color='b')
-        plot.pause(.5)
         # Now that we have the heading, introduce some noise into the SoG
         SoG = SoG + noise(SoG * noise_fact)  # Alter the SoG by +/- noise_fact of the original value
         headings.append(heading)
         SoGs.append(SoG)
-        input('Press enter')
+        if display_vectors:
+            plot_vectors(curr, prev, delta_vector, heading, heading_noise)
+        prev = curr # Reset the pointers for the next iteration
     return headings, SoGs
 
 
+def plot_vectors(curr, prev, delta_vector, heading, heading_noise):
+    plot.scatter(curr[0], curr[1], color='r')
+    plot.scatter(prev[0], prev[1], color='b')
+    plot.pause(.5)
+    plot_delta_vector(curr, delta_vector)
+    plot_noise_vector(curr, heading_noise)
+    plot_heading_vector(curr, heading)
+
+
+def plot_heading_vector(curr, heading):
+    plot.axes().quiver(curr[0], curr[1], heading[0], heading[1], angles='xy', scale_units='xy', scale=1, color='b')
+    plot.pause(.5)
+
+
+def plot_noise_vector(curr, heading_noise):
+    plot.axes().quiver(curr[0], curr[1], heading_noise[0], heading_noise[1], angles='xy', scale_units='xy', scale=1,
+                       color='k')
+    plot.pause(.5)
+
+
+def plot_delta_vector(curr, delta_vector):
+    plot.axes().quiver(curr[0], curr[1], delta_vector[0], delta_vector[1], angles='xy', scale_units='xy', scale=1,
+                       color='g')
+    plot.pause(.5)
+
+
 '''
-Generate semi noisy ais data, based on pre-generated location data
+Generate semi noisy ais ais_data, based on pre-generated location ais_data
 '''
 def gen_test_ais_data(loc_data, noise_fact=0.1):
     head_data, sog_data = gen_head_and_SoG_data_from_loc(loc_data, noise_fact)
@@ -82,9 +103,9 @@ def gen_test_ais_data(loc_data, noise_fact=0.1):
 
 
 '''
-Generate semi noisy ais data, complete with location, heading, and SoG measurements, based on a mathematical function
+Generate semi noisy ais ais_data, complete with location, heading, and SoG measurements, based on a mathematical function
 '''
-def gen_random_data(b_func=sin_1_4th, noise_fact=0.2, num_points=100):
+def gen_random_data(b_func=sin_1_4th, noise_fact=0.01, num_points=100):
     loc_data = gen_loc_data(b_func, noise_fact, num_points)
     return gen_test_ais_data(loc_data, noise_fact)
 
