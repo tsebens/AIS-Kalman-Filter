feea7e1635361b4b4d5b7ec8de758676c734fd28
tristan.sebens <trisan.ng.sebens@gmail.com>
2018-03-23 14:03:09 -0800

    Unspecific refactoring

--- bin/h_g_filter.py
+++ bin/h_g_filter.py
@@ -1,36 +1,15 @@
 # Kalman filter
-from convert import make_initial_state, make_state_from_deprecated_ais_data_format as make_state
-from estimation import default_SoG_estimate, defalt_heading_estimate, default_location_estimate
-from prediction import default_SoG_prediction, default_heading_prediction, default_location_prediction
+from convert import make_initial_state
+from state import FilterState
 
 '''
 Executes a Kalman filter on the passed ais_data and returns a list of estimates for location(lat/lon), heading, and SoG for every point in ais_data after the first.
 The first point is considered to be completely accurate. Gotta start somewhere.
-Data is an nx4 numpy array, each row of which == (loc, heading, SoG, time)
-loc must be a numpy array, representing Lat/Lon (lat, lon)
-Heading must be a unit vector (length 1), and must come in as a numpy array (lat, lon)
-SoG must be in DecDeg/sec
-time must be an aware datetime object (timezone aware)
-loc_, head_, and SoG_ fact's' represent the consideration factors for each value. Essentially a measure of the proportion 
-of the final estimate that will be made up of the measurement. 
-A high fact will result in a higly influential measurement: 
-the final estimates will be very faithful to the measurements. 
-A low fact will result in a very uninfluential measurement: 
-the final estimates will be far more closely tied to the predictions made prior to the estimates.
 
-In simpler terms, think of fact as a metric of how trustworthy a set of measurements are: A low fact is good for 
-measurements that are error prone. A high fact is good for measurements that tend to be accurate.
-
-The prediction and estimation functions are used to provide the predictions and the estimates for the new values 
-of the location, heading, and speed, based on the relevant previous values.
-By defining these as passed functions with default values, it becomes possible to inject the logi which 
-the filter uses to make it's predictions and estimates.
+The filter_state is a value object which contains a lot of information about the way in which the filter should behave
+Refer to the FilterState object documentation for a more complete description.
 '''
-def ais_kalman(data, filter_state):
-    pred_location, pred_heading, pred_SoG = init_prediction_functions(filter_state)
-    est_location, est_heading, est_SoG = init_estimation_function(filter_state)
-    # Initialize our lists for storing the results of the filter
-    loc_predictions, head_predictions, SoG_predictions, loc_estimates, head_estimates, SoG_estimates = [], [], [], [], [], []
+def ais_kalman(data, filter_state: FilterState):
     vessel_states = []
     # Populate our initial values
     prev_state = make_initial_state(data[0])
@@ -64,18 +43,3 @@ def prediction_step(curr_state, filter_state, prev_state):
         curr_state, prev_state)
     curr_state.SoG_state.pred = filter_state.SoG_functions.predict(
         curr_state, prev_state)
-
-
-def init_prediction_functions(filter_state):
-    l_p = filter_state.location_functions.predict
-    h_p = filter_state.heading_functions.predict
-    s_p = filter_state.SoG_functions.predict
-    return l_p, h_p, s_p
-
-
-def init_estimation_function(filter_state):
-    l_e = filter_state.location_functions.estimate
-    h_e = filter_state.heading_functions.estimate
-    s_e = filter_state.SoG_functions.estimate
-    return l_e, h_e, s_e
-
--- bin/main.py
+++ bin/main.py
@@ -1,15 +1,44 @@
+from convert import make_state_from_deprecated_ais_data_format
 from data import gen_random_data, sin_1_4th
+from estimation import default_SoG_estimate, default_location_estimate, defalt_heading_estimate
 from h_g_filter import ais_kalman
 from plot import make_iterative_plot
+from prediction import default_SoG_prediction, default_heading_prediction, default_location_prediction
+from state import FactorState, FilterState, FunctionState
 
 loc_fact = 0.2
 head_fact = 0.4
 SoG_fact =0.4
 
+factor_state = FactorState(
+    loc_fact,
+    head_fact,
+    SoG_fact
+)
+
+location_functions = FunctionState(
+    predict=default_location_prediction,
+    estimate=default_location_estimate
+)
+
+heading_functions = FunctionState(
+    predict=default_heading_prediction,
+    estimate=defalt_heading_estimate
+)
+
+SoG_functions = FunctionState(
+    predict=default_SoG_prediction,
+    estimate=default_SoG_estimate
+)
+
+filter_state = FilterState(
+    factor_state,
+    location_functions,
+    heading_functions,
+    SoG_functions
+)
 
 data = gen_random_data()
-loc_data = [row[0] for row in data]
-head_data = [row[1] for row in data]
-sog_data = [row[2] for row in data]
-vessel_states = ais_kalman(data, loc_fact, head_fact, SoG_fact)
+states = [make_state_from_deprecated_ais_data_format(point) for point in data]
+vessel_states = ais_kalman(states, filter_state)
 make_iterative_plot([state.loc_state for state in vessel_states], b_func=sin_1_4th)
