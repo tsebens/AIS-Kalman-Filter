d781c54061c8315ecaee142a2f82f81109adc5f5
tristan.sebens <trisan.ng.sebens@gmail.com>
2018-03-22 17:00:48 -0800

    Refactored default prediction functions to be able to use both raw values and state value objects.
    Basically just getting ready to gingerly shift from the former to the latter.

--- bin/prediction.py
+++ bin/prediction.py
@@ -15,18 +15,27 @@ Because of this, whenever the functions are called, the parameters must be expli
 '''
 The default prediction functions
 '''
-def default_SoG_prediction(loc_est=None, head_est=None, SoG_est=None):
-    SoG_pred = SoG_est  # We assume that vessels maintain their speed over time.
+def default_SoG_prediction(loc_est=None, head_est=None, SoG_est=None, curr_state=None, prev_state=None):
+    if prev_state==None:
+        SoG_pred = SoG_est  # We assume that vessels maintain their speed over time.
+    else:
+        SoG_pred = prev_state.SoG_state.est # We assume that vessels maintain their speed over time.
     return SoG_pred
 
 
-def default_heading_prediction(loc_est=None, head_est=None, SoG_est=None):
-    head_pred = head_est  # We assume that vessels maintain their heading over time.
+def default_heading_prediction(loc_est=None, head_est=None, SoG_est=None, curr_state=None, prev_state=None):
+    if prev_state==None:
+        head_pred = head_est  # We assume that vessels maintain their heading over time.
+    else:
+        head_pred = prev_state.head_state.est
     return head_pred
 
 
-def default_location_prediction(loc_est=None, head_est=None, SoG_est=None):
-    loc_pred = np.add(loc_est, head_est * SoG_est)
+def default_location_prediction(loc_est=None, head_est=None, SoG_est=None, curr_state=None, prev_state=None):
+    if curr_state==None or prev_state==None:
+        loc_pred = np.add(loc_est, head_est * SoG_est)
+    else:
+        loc_pred = np.add(prev_state.loc_state.est, prev_state.head_state.est * prev_state.SoG_state.est)
     return loc_pred
 
 
@@ -34,6 +43,9 @@ def default_location_prediction(loc_est=None, head_est=None, SoG_est=None):
 '''
 Custom behaviour prediction functions
 '''
-# Enforces a strict rule that the estimated SoG of the vessel cannot exceed 15 knots
-def SoG_prediction_15knt_max(SoG_est):
-    return SoG_est if SoG_est < knts_to_mps(15) else knts_to_mps(15)
+# Enforces a strict rule that the estimated SoG of the vessel cannot exceed max_spd knots
+def SoG_prediction_max_spd(loc_est=None, head_est=None, SoG_est=None, max_spd=15):
+    return SoG_est if SoG_est < knts_to_mps(max_spd) else knts_to_mps(max_spd)
+
+
+
