628c77f56e7d8bea1afc747acb5048f61db6f3ab
tristan.sebens <trisan.ng.sebens@gmail.com>
2018-03-23 12:08:44 -0800

    Refactored main to reflect the use of states.

--- bin/main.py
+++ bin/main.py
@@ -11,5 +11,5 @@ data = gen_random_data()
 loc_data = [row[0] for row in data]
 head_data = [row[1] for row in data]
 sog_data = [row[2] for row in data]
-loc_estimates, loc_predictions, head_estimates, head_predictions, SoG_estimates, SoG_predictions = ais_kalman(data, loc_fact, head_fact, SoG_fact)
-make_iterative_plot(loc_data, loc_predictions, loc_estimates, b_func=sin_1_4th)
+vessel_states = ais_kalman(data, loc_fact, head_fact, SoG_fact)
+make_iterative_plot([state.loc_state for state in vessel_states], b_func=sin_1_4th)
