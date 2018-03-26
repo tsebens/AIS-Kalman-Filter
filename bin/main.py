4c6e5d1140ece2820e25ab14908996992bbdaf98
tristan.sebens <trisan.ng.sebens@gmail.com>
2018-03-26 09:17:32 -0800

    The results of about a week of testing. This main generates some noisy data, then runs it through the filter to clean it up, and finally plots the whole mess for your judgment.

--- bin/main.py
+++ bin/main.py
@@ -1,6 +1,7 @@
 from convert import make_state_from_deprecated_ais_data_format
-from data import gen_random_data, sin_1_4th
-from estimation import default_SoG_estimate, default_location_estimate, defalt_heading_estimate
+from gen import gen_random_data, sin_1_4th
+from estimation import default_SoG_estimate, default_location_estimate, defalt_heading_estimate, est_head_max_turn, \
+    est_SoG_max_spd
 from h_g_filter import ais_kalman
 from plot import make_iterative_plot
 from prediction import default_SoG_prediction, default_heading_prediction, default_location_prediction
@@ -11,9 +12,9 @@ head_fact = 0.4
 SoG_fact =0.4
 
 factor_state = FactorState(
-    loc_fact,
-    head_fact,
-    SoG_fact
+    loc_fact=0.5,
+    head_fact=0.2,
+    SoG_fact=0.2
 )
 
 location_functions = FunctionState(
@@ -23,12 +24,12 @@ location_functions = FunctionState(
 
 heading_functions = FunctionState(
     predict=default_heading_prediction,
-    estimate=defalt_heading_estimate
+    estimate=est_head_max_turn
 )
 
 SoG_functions = FunctionState(
     predict=default_SoG_prediction,
-    estimate=default_SoG_estimate
+    estimate=est_SoG_max_spd
 )
 
 filter_state = FilterState(
@@ -42,3 +43,4 @@ data = gen_random_data()
 states = [make_state_from_deprecated_ais_data_format(point) for point in data]
 vessel_states = ais_kalman(states, filter_state)
 make_iterative_plot([state.loc_state for state in vessel_states], b_func=sin_1_4th)
+input('Press enter')
