f48ea7b32a0d3db165de7aeafd35f8eea9e22dde
tristan.sebens <trisan.ng.sebens@gmail.com>
2018-03-26 09:15:34 -0800

    Added variable for max vessel speed

--- bin/configuration.py
+++ bin/configuration.py
@@ -7,4 +7,7 @@ configuration options that the user may want to change prior to running the appl
 
 # The maximum number of degrees the average fishing vessel is capable of turning per second.
 # Used when estimating a vessel's heading.
-MAX_ALLOWABLE_HEADING_CHANGE_DEGREES_PER_SECOND = 5
+MAX_ALLOWABLE_HEADING_CHANGE_DEGREES_PER_SECOND = 1
+
+# Max speed of the average fishing vessel in knts
+MAX_ALLOWABLE_VESSEL_SPEED = 15
