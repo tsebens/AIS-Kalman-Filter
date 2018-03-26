982d183eee3ca01f4a2fb0354fdde19485e9fe9d
tristan.sebens <trisan.ng.sebens@gmail.com>
2018-03-25 18:49:44 -0800

    Added a config file. Still need to move several things to it.

---
+++ bin/configuration.py
@@ -0,0 +1,10 @@
+'''
+Configuration file.
+
+Intended as a repository of constant variables and
+configuration options that the user may want to change prior to running the application.
+'''
+
+# The maximum number of degrees the average fishing vessel is capable of turning per second.
+# Used when estimating a vessel's heading.
+MAX_ALLOWABLE_HEADING_CHANGE_DEGREES_PER_SECOND = 5
