39f73105bbfd04e2f9147b2df20b72ada12439b6
tristan.sebens <trisan.ng.sebens@gmail.com>
2018-04-10 11:35:29 -0800

    Updated config file with values from Chandler

--- bin/configuration.py
+++ bin/configuration.py
@@ -7,20 +7,20 @@ configuration options that the user may want to change prior to running the appl
 
 Note: NOT ALL OF THESE ARE ALWAYS USED
 '''
+# The number of degrees a fishing vessel can change it's heading by, per second
+AVERAGE_HEADING_CHANGE_DEGREES_PER_SECOND = 3
+MAX_ALLOWABLE_HEADING_CHANGE_DEGREES_PER_SECOND = 6
+# Maximum number of degrees a vessel is allowed to change it's heading by between consecutive states
+MAX_ALLOWABLE_TURN_PER_STATE = 25
 
-# The maximum number of degrees the average fishing vessel is capable of turning per second.
-# Used when estimating a vessel's heading.
-
-MAX_ALLOWABLE_HEADING_CHANGE_DEGREES_PER_SECOND = .0000001
-
-# Max speed of the average fishing vessel in m/s
+AVERAGE_VESSEL_SPEED = 3.85833 # 7.5 knots
 MAX_ALLOWABLE_VESSEL_SPEED = 7.71667 # We'll set it at 15 knots, which is this (according to google)
 
 # Max acceleration of the average fishing vessel
+AVERAGE_VESSEL_ACCELERATION = 0.02
 MAX_ALLOWABLE_VESSEL_ACCELERATION_METERS_PER_SECOND = 0.5
 
-# Maximum number of degrees a vessel is allowed to change it's heading by between consecutive states
-MAX_ALLOWABLE_TURN_PER_STATE = 25
+
 '''
 Constants
 '''
