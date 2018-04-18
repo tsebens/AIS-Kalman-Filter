72326f453c796ad91f3877ed7531f383159cf6af
tristan.sebens <trisan.ng.sebens@gmail.com>
2018-04-18 15:03:25 -0800

    Moved some more constant values into the db conf file

--- conf/db.py
+++ conf/db.py
@@ -11,6 +11,14 @@ dbname = 'kalman'
 user = 'tristan.sebens'
 pwd = ''
 
+# The table where the filter will look for inputs
+OUTPUT_TABLE = Table('VMS_TEST_VOYAGE_FILTERED', schema='public')
+# The table where the filter will write it's outputs
+INPUT_TABLE = Table('VMS_TEST_VOYAGE', schema='public')
+# The field the filter will use to distinguish individual vessels
+ID_FIELD = Field("VESSEL_ID")
+
+
 # The DB table where column names are stored.
 DB_COLUMNS_TABLE = Table('columns', schema='information_schema') # Configured for PostgreSQL DB
 # The field in the column name table that stores the column names
