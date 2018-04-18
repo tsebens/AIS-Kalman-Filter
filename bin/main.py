62fbc5c7b6498db48bd504989fa6f70a666475e4
tristan.sebens <trisan.ng.sebens@gmail.com>
2018-04-18 15:03:35 -0800

    Trimming and testing

--- bin/main.py
+++ bin/main.py
@@ -1,18 +1,19 @@
-from pypika import Table, Field
+from pypika import Table
 from conf.db import server, port, dbname, user, pwd
 from conf.filter import filter_state
 from connect import TableVessel, PostgreSQLDataBase
-from plot import make_iterative_plot
+from db import ID_FIELD, OUTPUT_TABLE, INPUT_TABLE
 from vms import VMSDataPackage
 from filter import ais_kalman
 
+db = PostgreSQLDataBase(server, port, dbname, user, pwd)
 
-db_spec = PostgreSQLDataBase(server, port, dbname, user, pwd)
-conn = db_spec.get_connection()
-in_table = Table('VMS_TEST_VOYAGE')
-out_table = Table("VMS_TEST_VOYAGE_FILTERED")
-in_table_vessel = TableVessel(conn, in_table, id_field=Field("VESSEL_ID"), id_value=2791)
-out_table_vessel = TableVessel(conn, out_table, id_field=Field("VESSEL_ID"), id_value=2791)
+in_table = Table(INPUT_TABLE)
+out_table = Table(OUTPUT_TABLE)
+ids = db.get_ids(in_table, ID_FIELD)
+conn = db.get_connection()
+in_table_vessel = TableVessel(conn, in_table, id_field=ID_FIELD, id_value=2791)
+out_table_vessel = TableVessel(conn, out_table, id_field=ID_FIELD, id_value=2791)
 data_package = VMSDataPackage(in_tbl_vessel=in_table_vessel, out_tbl_vessel=out_table_vessel)
 data_package.load_payload()
 vessel_states = ais_kalman(data_package.get_states(), filter_state)
