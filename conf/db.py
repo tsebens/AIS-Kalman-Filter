05454a1e57b45c41878572fe13ae7b5077c58981
--global <trisan.ng.sebens@gmail.com>
2018-09-04 18:45:50 -0800

    Refactored the db object definition from main.py to the db conf file.

--- conf/db.py
+++ conf/db.py
@@ -1,34 +1,45 @@
 from pypika import Table, Field
 
+from connect import SQLServerDataBase, PostgreSQLDataBase
+
 """
 ----------------------------------------------------------------
 DATABASE CONFIGURATION
 ----------------------------------------------------------------
 """
-server = r'akr-j53\PROD'
-port = '1433'
-dbname = 'CATCH_IN_AREAS'
-user = ''
+server = r'localhost'
+port = '5432'
+dbname = 'kalman'
+user = 'tristan.sebens'
 pwd = ''
+trusted = False # Tells the connection to use OS authentication or not
+
+"""
+----------------------------------------------------------------
+DATABASE CONNECTION
+----------------------------------------------------------------
+The database connection produced by the specifications contained in this file
+"""
+db = PostgreSQLDataBase(server, port, dbname, user, pwd, trusted_source=trusted)
 
 """
 ----------------------------------------------------------------
 INPUT
 ----------------------------------------------------------------
 """
-INPUT_TABLE = Table("STG_VMS", schema='dbo')
+INPUT_TABLE = Table("VMS_TEST_VOYAGE", schema='public')
 # The field the filter will use to distinguish individual vessels
 ID_FIELD_NAME = "VESSEL_ID"
-ID_FIELD = Field(ID_FIELD_NAME)
+ID_FIELD = Field(ID_FIELD_NAME, table=INPUT_TABLE)
 # Fieldname for the longitude values
 LON_FIELD_NAME = "LONGITUDE"
-LON_FIELD = Field(LON_FIELD_NAME)
+LON_FIELD = Field(LON_FIELD_NAME, table=INPUT_TABLE)
 # Fieldname for the latitude values
 LAT_FIELD_NAME = "LATITUDE"
-LAT_FIELD = Field(LAT_FIELD_NAME)
+LAT_FIELD = Field(LAT_FIELD_NAME, table=INPUT_TABLE)
 # Fieldname for the timestamp values
 TIMESTAMP_FIELD_NAME = "POSITION_DATETIME"
-TIMESTAMP_FIELD = Field(TIMESTAMP_FIELD_NAME)
+TIMESTAMP_FIELD = Field(TIMESTAMP_FIELD_NAME, table=INPUT_TABLE)
 
 
 """
@@ -36,13 +47,16 @@ TIMESTAMP_FIELD = Field(TIMESTAMP_FIELD_NAME)
 OUTPUT
 ----------------------------------------------------------------
 """
-OUTPUT_TABLE = Table("stg_vms_test", schema='dbo')
+OUTPUT_TABLE = Table("VMS_TEST_VOYAGE_FILTERED", schema='public')
 # Fieldname for the output longitude values
-OUTPUT_LON_FIELD_NAME = "filt_lon"
+OUTPUT_LON_FIELD_NAME = "FILT_LON"
+OUTPUT_LON_FIELD = Field(OUTPUT_LON_FIELD_NAME, table=OUTPUT_TABLE)
 # Fieldname for the output latitude values
-OUTPUT_LAT_FIELD_NAME = "filt_lat"
+OUTPUT_LAT_FIELD_NAME = "FILT_LAT"
+OUTPUT_LAT_FIELD = Field(OUTPUT_LAT_FIELD_NAME, table=OUTPUT_TABLE)
 # Fieldname for the output deviance values
-OUTPUT_DEV_FIELD_NAME = "deviance"
+OUTPUT_DEV_FIELD_NAME = "DEVIANCE"
+OUTPUT_DEV_FIELD = Field(OUTPUT_DEV_FIELD_NAME, table=OUTPUT_TABLE)
 
 """
 ----------------------------------------------------------------
